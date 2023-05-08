e7aa4c8af7d1fca8b27813b460b345113ba6e703
Shin Soon <sweetejr777@gmail.com>
2023-05-08 21:48:40 +0900

    feat: CustomAuthenticationProvider

--- src/main/java/io/security/corespringsecurity/CoreSpringSecurityApplication.java
+++ src/main/java/io/security/corespringsecurity/CoreSpringSecurityApplication.java
@@ -1,14 +1,30 @@
 package io.security.corespringsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class CoreSpringSecurityApplication {
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    @PostConstruct
+    public void init(){
+        String pwd = passwordEncoder.encode("1");
+        jdbcTemplate.execute("INSERT INTO account (age, email, password, role, username, id) VALUES ('1','sweetejr777@gmail.com','"
+                + pwd
+                + "', 'ROLE_USER', 'user',next value for hibernate_sequence)");
+    }
 
     public static void main(String[] args) {
 
